3b923cd33363e6c911f30670b2b9f8bc3b13c007
ThindalTV <thindaltv@gmail.com>
2021-11-20 21:36:54 +0100

    Adding toggle command and making the hiding actually toggleable.

---
+++ src/NoDoxx/Commands/ToggleDisplayCommand/ToggleDisplayCommand.cs
@@ -0,0 +1,104 @@
+﻿using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Threading;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Task = System.Threading.Tasks.Task;
+
+namespace NoDoxx.Commands
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class ToggleDisplayCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("45b8c6de-75fb-4667-b5a2-47c9620c3dea");
+
+        public static bool DisplayConfigValues = false;
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToggleDisplayCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private ToggleDisplayCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static ToggleDisplayCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in ToggleDisplayCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new ToggleDisplayCommand(package, commandService);
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.JoinableTaskFactory.Run(async () => await ExecuteAsync());
+        }
+
+        private async Task ExecuteAsync()
+        {
+            // Force us back to the main thread
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+            WindowFrame windowFrame = await VS.Windows.GetCurrentWindowAsync();
+            if (windowFrame == null) return;
+
+            // Actually found a window and ready to find the adorner...
+            var documentView = await windowFrame.GetDocumentViewAsync();
+            var tw = documentView.TextView;
+            var adornerLayer = tw.GetAdornmentLayer("ConfigurationHiderAdorner");
+            DisplayConfigValues = !DisplayConfigValues;
+            adornerLayer.Opacity = DisplayConfigValues ? 1 : 0;
+        }
+    }
+}
